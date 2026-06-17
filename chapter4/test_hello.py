e756d4e65aa3a1ce09a53e777267a0d4ff66e0ae
z8amer <z8amer@gmail.com>
2026-06-17 16:45:05 +0000

    matched python outputs

--- chapter4/test_hello.py
+++ chapter4/test_hello.py
@@ -4,4 +4,4 @@ from hello import say_hello
 
 class TestHello(unittest.TestCase):
     def test_say_hello(self):
-        self.assertEqual(say_hello(), "Hello, World")
+        self.assertEqual(say_hello(), "Hello World!")
